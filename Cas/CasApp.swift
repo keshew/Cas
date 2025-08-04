6d2b9ed6b42f1a3424e6b11ddd09ac7ab4b45350
keshew <artyom.korotckov2017@yandex.ru>
2025-08-04 15:52:05 +0500

    Initial Commit

---
+++ Cas/CasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasApp.swift
+//  Cas
+//
+//  Created by Артём Коротков on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
